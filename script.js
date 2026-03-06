69277a84b2515ce1d0ddfc722f9966aa60553550
Raghav Soni <181562819+RaghavXE@users.noreply.github.com>
2026-03-06 21:22:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+    const locomotiveScroll = new LocomotiveScroll();
+    // Shery.mouseFollower();
+    
+//     {
+//   //Parameters are optional.
+//   skew: true,
+//   ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+//   duration: 1,
+// });
+
+
+// Shery.mouseFollower(".images", {
+//   //Parameters are optional.
+  
+//   skew: true,
+//   ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+//   duration: 1,
+// });
